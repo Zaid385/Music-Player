7e84f105c0ab28f8a138eba7ecc45495ab6a8bc3
Zaid385 <126477075+Zaid385@users.noreply.github.com>
2025-06-08 20:19:07 +0500

    Update Register.java

--- Register.java
+++ Register.java
@@ -79,9 +79,6 @@ public class Register extends Validation {
         stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
         scene = new Scene(root);
 
-        // css-here
-        scene.getStylesheets().add(getClass().getResource("styles.css").toExternalForm());
-
         stage.setScene(scene);
         stage.show();
     }
